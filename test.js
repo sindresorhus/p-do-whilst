f222bbbf6a29e80e30eff8b75efea017cc83955e
Gabriel Montes <gabriel@utoctadel.com.ar>
2024-08-29 12:01:38 -0400

    Accept an initial value (#6)
    Co-authored-by: Sindre Sorhus <sindresorhus@gmail.com>

--- test.js
+++ test.js
@@ -73,3 +73,40 @@ test('stops on error', async t => {
 	t.is(counter, 7);
 	t.deepEqual(result, [0, 1, 2, 3, 4, 5, 6]);
 });
+
+test('accepts an initial value', async t => {
+	const result = [];
+
+	const counter = await pDoWhilst(
+		currentCount => {
+			result.push(currentCount);
+			return currentCount + 1;
+		},
+		() => result.length < 7,
+		2
+	);
+
+	t.is(counter, 9);
+	t.deepEqual(result, [2, 3, 4, 5, 6, 7, 8]);
+});
+
+test('allows handling rejected promises within action', async t => {
+	const result = [];
+
+	const counter = await pDoWhilst(
+		async currentCount => {
+			try {
+				const resolvedCount = await currentCount;
+				result.push(resolvedCount);
+				return resolvedCount + 1;
+			} catch {
+				return Promise.resolve(2);
+			}
+		},
+		() => result.length < 3,
+		Promise.reject(new Error('test error'))
+	);
+
+	t.is(counter, 5);
+	t.deepEqual(result, [2, 3, 4]);
+});
